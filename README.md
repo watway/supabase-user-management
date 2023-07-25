aaf9a72b0e9f6ad81f9c93e44957ad03b88d333c
Craig Watson <craig@watway.net>
2023-07-25 18:04:31 +1000

    progress through building the app

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+https://supabase.com/docs/guides/getting-started/tutorials/with-nextjs
+https://github.com/supabase/supabase/tree/master/examples/user-management/nextjs-user-management
+
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
---
+++ src/app/auth/signout/route.ts
@@ -0,0 +1,18 @@
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
+import { NextRequest, NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
+
+export async function POST(req: NextRequest) {
+  const supabase = createRouteHandlerClient({ cookies });
+
+  // Check if we have a session
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (session) {
+    await supabase.auth.signOut();
+  }
+
+  return NextResponse.redirect(new URL('/', req.url), { status: 302 });
+}
